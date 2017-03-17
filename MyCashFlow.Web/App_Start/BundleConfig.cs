5239a7e818fab3bb97e4d13cfbc96ac0de3f3ce9
Marsellus47 <marsellus47@users.noreply.github.com>
2017-03-17 20:16:59 +0100

    Bundles
    Bundles

---
+++ MyCashFlow.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Optimization;
+
+namespace MyCashFlow.Web
+{
+	public static class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			// Script bundles
+			bundles.Add(new ScriptBundle(Bundles.Scripts.Jquery)
+				.Include("~/Scripts/jquery-{version}.js"));
+
+			bundles.Add(new ScriptBundle(Bundles.Scripts.Jqueryval)
+				.Include("~/Scripts/jquery.validate*"));
+
+			bundles.Add(new ScriptBundle(Bundles.Scripts.Modernizr)
+				.Include("~/Scripts/modernizr-*"));
+
+			bundles.Add(new ScriptBundle(Bundles.Scripts.Bootstrap)
+				.Include("~/Scripts/bootstrap.min.js",
+						 "~/Scripts/respond.js"));
+
+			// Style bundles
+			bundles.Add(new StyleBundle(Bundles.Styles.Css).Include(
+					"~/Content/bootstrap.css",
+					"~/Content/Site.css"));
+		}
+	}
+
+	public static class Bundles
+	{
+		public static class Scripts
+		{
+			public const string Jquery = "~/bundles/jquery";
+			public const string Jqueryval = "~/bundles/jqueryval";
+			public const string Modernizr = "~/bundles/modernizr";
+			public const string Bootstrap = "~/bundles/bootstrap";
+		}
+
+		public static class Styles
+		{
+			public const string Css = "~/Content/css";
+		}
+	}
+}
--- MyCashFlow.Web/Global.asax.cs
+++ MyCashFlow.Web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using MyCashFlow.Identity.Initializer;
 using System.Data.Entity;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MyCashFlow.Web
@@ -12,6 +13,7 @@ namespace MyCashFlow.Web
             AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
 			Database.SetInitializer(new DatabaseInitializer());
         }
     }
